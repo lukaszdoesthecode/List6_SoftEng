8f791a85b1e13a1499c9c89e27bf3a4267d89453
Lukasz Stecyk <stecyk.lukasz@gmail.com>
2025-01-28 20:06:00 +0100

    error fixed

--- list5_SE/settings.py
+++ list5_SE/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
-#INSTALLED_APPS = [
+INSTALLED_APPS = [
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
